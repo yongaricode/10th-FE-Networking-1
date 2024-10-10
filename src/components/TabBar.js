39a08296c465db7a25a0e144b368a988ab60d28c
yongaricode <kang061313@gmail.com>
2024-10-10 17:32:41 +0900

    카테고리바 동작 오류 수정 및 tabBar UI 구현

---
+++ src/components/TabBar.js
@@ -0,0 +1,21 @@
+function TabBar() {
+  const tabBar = document.createElement("div");
+  tabBar.id = "tab-bar";
+
+  tabBar.innerHTML = `
+    <div id='press-name'>
+    <p>전체 언론사</p>
+    <p>내가 구독한 언론사</p></div>
+    <div id='list-gird'>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path d="M19 5V19H5V5H19ZM20.1 3H3.9C3.4 3 3 3.4 3 3.9V20.1C3 20.5 3.4 21 3.9 21H20.1C20.5 21 21 20.5 21 20.1V3.9C21 3.4 20.5 3 20.1 3ZM11 7H17V9H11V7ZM11 11H17V13H11V11ZM11 15H17V17H11V15ZM7 7H9V9H7V7ZM7 11H9V13H7V11ZM7 15H9V17H7V15Z" fill="#4362D0"/>
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path d="M3 11V3H11V11H3ZM3 21V13H11V21H3ZM13 11V3H21V11H13ZM13 21V13H21V21H13ZM5 9H9V5H5V9ZM15 9H19V5H15V9ZM15 19H19V15H15V19ZM5 19H9V15H5V19Z" fill="#879298"/>
+    </svg>
+    </div>`;
+
+  return tabBar;
+}
+
+export default TabBar;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import createCategoryBar from "./components/categoryBar.js";
 import header from "./components/Header.js";
 import rollingNews from "./components/RollingNews.js";
 import PressNews from "./components/PressNews.js";
+import tabBar from "./components/TabBar.js";
 
 export const index = () => {
   const app = document.getElementById("app");
+  const categoryBar = createCategoryBar();
 
   app.innerHTML = `
     ${header().outerHTML}
@@ -12,11 +14,13 @@ export const index = () => {
       ${rollingNews().outerHTML}
       ${rollingNews().outerHTML}
     </div>
+    ${tabBar().outerHTML}
     <div id="news-list">
-      ${createCategoryBar().outerHTML}
       ${PressNews().outerHTML}
     </div>
   `;
+
+  app.querySelector("#news-list").appendChild(categoryBar);
 };
 
 index();
