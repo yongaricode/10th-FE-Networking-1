a3c277fc573f0331404d22c87523637e9bff9a95
yongaricode <kang061313@gmail.com>
2024-10-29 18:05:33 +0900

    feat: PressNews UI 구현

--- src/components/PressNews.js
+++ src/components/PressNews.js
@@ -1,6 +1,10 @@
+import { createElement } from "../utils/createElement.js";
+
 function PressNews() {
   const pressNews = document.createElement("div");
   pressNews.id = "press-news";
+
+  let current;
 
   function testFetchJSON() {
     const dataPath = "/src/data/news.json";
@@ -13,13 +17,66 @@ function PressNews() {
         return response.json();
       })
       .then((data) => {
-        console.log("JSON data:", data); // JSON 데이터를 콘솔에 출력
+        current = data[Object.keys(data)[0]];
+        console.log(current);
+
+        loadMainNews();
       })
       .catch((error) => console.error("Error loading JSON:", error));
   }
 
   // 함수 호출
   testFetchJSON();
+
+  function loadMainNews() {
+    const pressInfo = document.createElement("div");
+    pressInfo.id = "press-info";
+
+    const mediaLogo = createElement("", "media-logo", "img");
+    mediaLogo.src = current.logoLight;
+    pressInfo.appendChild(mediaLogo);
+
+    const editTime = createElement(current.time, "time", "span");
+    pressInfo.appendChild(editTime);
+
+    pressNews.appendChild(pressInfo);
+
+    const News = document.createElement("div");
+    News.id = "news";
+    const Main = document.createElement("div");
+    Main.id = "main";
+    const Sub = document.createElement("div");
+    Sub.id = "sub";
+
+    News.appendChild(Main);
+
+    const thumbNail = createElement("", "mainImg", "img");
+    thumbNail.src = current.mainImg;
+    Main.appendChild(thumbNail);
+
+    const title = createElement(current.mainTitle, "mainTitle", "span");
+    Main.appendChild(title);
+
+    const subtitleList = document.createElement("ul");
+
+    current.relatedArticles.forEach((subtitle) => {
+      const listItem = document.createElement("li");
+      listItem.className = "subtitle";
+      listItem.textContent = subtitle.title;
+      subtitleList.appendChild(listItem);
+    });
+
+    const caption = createElement(
+      `${current.mediaName} 언론사에서 직접 편집한 뉴스입니다.`,
+      "caption",
+      "span"
+    );
+
+    Sub.appendChild(subtitleList);
+    Sub.appendChild(caption);
+    News.appendChild(Sub);
+    pressNews.appendChild(News);
+  }
 
   return pressNews;
 }
